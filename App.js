30c565eb55d1fc7cf2765401e1511ab6b9470d2f
tutto-bello <muller.gyorgy.gergely@gmail.com>
2023-01-09 06:33:49 +0100

    clean

--- App.js
+++ App.js
@@ -1,68 +1,12 @@
-import { StyleSheet, View, FlatList, Button } from "react-native";
-import { useState } from "react";
+import { StyleSheet, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
-import GoalItem from "./components/GoalItem";
-import GoalInput from "./components/GoalInput";
-
 export default function App() {
-  const [modalIsVisable, setModalIsVisable] = useState(false);
-  const [courseGoals, setCourseGoals] = useState([]);
-
-  function startAddGoalHandler() {
-    setModalIsVisable(true);
-  }
-
-  function endAddGoalHandler() {
-    setModalIsVisable(false);
-  }
-
-  const addGoalHandler = (enterText) => {
-    setCourseGoals((currentCourseGoals) => [
-      ...currentCourseGoals,
-      { text: enterText, key: Math.random().toString() },
-    ]);
-    setModalIsVisable(false);
-  };
-
-  function deleteGoalHanler(id) {
-    setCourseGoals((currentCourseGoals) => {
-      return currentCourseGoals.filter((item) => item.key !== id);
-    });
-  }
-
   return (
     <>
-      <StatusBar style="light" />
+      <StatusBar style="auto" />
       <View style={styles.appConatiner}>
-        <Button
-          title="Add new goal"
-          color="#a065ec"
-          onPress={startAddGoalHandler}
-        />
-        <GoalInput
-          addGoalHandler={addGoalHandler}
-          visible={modalIsVisable}
-          onCancel={endAddGoalHandler}
-        />
-        <View style={styles.goalsContainer}>
-          <FlatList
-            data={courseGoals}
-            alwaysBounceVertical={false}
-            keyExtractor={(item, index) => {
-              return item.key;
-            }}
-            renderItem={(itemData) => {
-              return (
-                <GoalItem
-                  text={itemData.item.text}
-                  id={itemData.item.key}
-                  deleteGoalHanler={deleteGoalHanler}
-                />
-              );
-            }}
-          />
-        </View>
+        <Text>Hellor there</Text>
       </View>
     </>
   );
