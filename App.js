87a7cb2a96947682e894f9d75d061a0b31c38a75
tutto-bello <muller.gyorgy.gergely@gmail.com>
2023-03-17 10:40:20 +0100

    fix(auth): register and login

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
@@ -6,6 +7,7 @@ import LoginScreen from "./screens/LoginScreen";
 import SignupScreen from "./screens/SignupScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import { Colors } from "./constants/styles";
+import AuthContextProvider, { AuthContext } from "./store/auth-context";
 
 const Stack = createNativeStackNavigator();
 
@@ -39,9 +41,12 @@ function AuthenticatedStack() {
 }
 
 function Navigation() {
+  const authCtx = useContext(AuthContext);
+
   return (
     <NavigationContainer>
-      <AuthStack />
+      {!authCtx.isAuthenticated && <AuthStack />}
+      {authCtx.isAuthenticated && <AuthenticatedStack />}
     </NavigationContainer>
   );
 }
@@ -50,8 +55,9 @@ export default function App() {
   return (
     <>
       <StatusBar style="light" />
-
-      <Navigation />
+      <AuthContextProvider>
+        <Navigation />
+      </AuthContextProvider>
     </>
   );
 }
--- util/auth.js
+++ util/auth.js
@@ -2,13 +2,24 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyDN5fluBLIGFG0tbyyLgFv7SZPyoyyPMAs";
 
-export async function createUser(email, password) {
-  const response = await axios.post(
-    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    }
-  );
+async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+
+  const token = response.data.idToken;
+
+  return token;
+}
+
+export function createUser(email, password) {
+  return authenticate("signUp", email, password);
+}
+
+export function login(email, password) {
+  return authenticate("signInWithPassword", email, password);
 }
